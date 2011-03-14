4a9f057f0a5fa8b1d1754ba45484373770373ddb
Joseph Klein <jfbguy@gmail.com>
2011-03-14 16:45:32 -0700

    rebase test

--- AudioRunner/src/edu/mills/cs280/audiorunner/DesktopStarter.java
+++ AudioRunner/src/edu/mills/cs280/audiorunner/DesktopStarter.java
@@ -3,6 +3,7 @@ package edu.mills.cs280.audiorunner;
 import com.badlogic.gdx.backends.jogl.JoglApplication;
 
 //Tested branch with this comment!!!
+//New comment, woot!
 
 public class DesktopStarter {
 	public static void main(String[] args){
