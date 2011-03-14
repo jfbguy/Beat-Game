3cee003aff7ce23e02ddc3fb57c6e54722ea1ecb
Joseph Klein <jfbguy@gmail.com>
2011-03-14 16:12:11 -0700

    just a branch test

--- AudioRunner/src/edu/mills/cs280/audiorunner/DesktopStarter.java
+++ AudioRunner/src/edu/mills/cs280/audiorunner/DesktopStarter.java
@@ -2,6 +2,8 @@ package edu.mills.cs280.audiorunner;
 
 import com.badlogic.gdx.backends.jogl.JoglApplication;
 
+//Tested branch with this comment!
+
 public class DesktopStarter {
 	public static void main(String[] args){
 		new JoglApplication(new GameHandler(),
