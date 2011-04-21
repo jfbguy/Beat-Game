70df56a43d097f1a4d5a431530e2bcb27b9294d9
Trevor <trevorbadams@gmail.com>
2011-04-21 16:04:59 -0700

    Added commented out test to load saved data in FindThreshTest

--- AudioRunner/src/edu/mills/cs280/audiorunner/FindThreshTest.java
+++ AudioRunner/src/edu/mills/cs280/audiorunner/FindThreshTest.java
@@ -4,6 +4,10 @@ public class FindThreshTest {
 	private static String TEST_FILE = "data/music/Freezepop - Starlight (Karacter Remix).mp3";
 	
 	public static void main( String[] argv ){
+//		SerializableMusicData test = SerializableMusicData.load();
+//		System.out.println(test.getPeaks());
+//		System.exit(7);
+		
 		PeakFinder george = new PeakFinder(TEST_FILE);
 		System.out.println(george.returnPeaks().toString());
 
