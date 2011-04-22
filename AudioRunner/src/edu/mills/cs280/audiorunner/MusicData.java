97ae0265de03f3e3235e495b23815128d22fb821
Joseph Klein <jfbguy@gmail.com>
2011-04-22 12:20:02 -0700

    trying to get data to load

--- AudioRunner/src/edu/mills/cs280/audiorunner/MusicData.java
+++ AudioRunner/src/edu/mills/cs280/audiorunner/MusicData.java
@@ -1,7 +1,12 @@
 package edu.mills.cs280.audiorunner;
 
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
+
+import com.badlogic.gdx.Gdx;
 
 import javazoom.jl.decoder.SampleBuffer;
 
@@ -10,40 +15,77 @@ public class MusicData {
 	private static List<Float> peaks;
 	private static ArrayList<float[]> samples;
 	private static int duration; //the length of the music in milisec
-	
+
 	private MusicData(){
-		
+
 	}
-	
+
 	public static void setSamples(ArrayList<float[]> loadedSamples){
 		samples = loadedSamples;
 	}
-	
+
 	public static void setFile(String file){
 		fileLocation = file;
 	}
-	
+
 	public static void setpeaks(List<Float> inPeaks){
 		peaks = inPeaks;
 	}
-	
+
 	public static List<Float> getPeaks(){
 		return peaks;
 	}
-	
+
 	public static ArrayList<float[]> getSamples(){
 		return samples;
 	}
-	
+
 	public static String getFileLocation(){
 		return fileLocation;
 	}
-	
+
 	public static int getDuration(){
 		return duration;
 	}
-	
+
 	public static float getFrameDuration(){
 		return duration/samples.size();
+	}
+
+	public static void loadBufferedData(){
+		System.out.println("TRYING TO LOAD!!!!");
+
+		try {
+			InputStream fis = Gdx.files.internal("data/testpeaks.ar").read();
+			ObjectInputStream in = new ObjectInputStream(fis);
+			@SuppressWarnings("unchecked")
+			List<Float> peaks = (List<Float>)in.readObject();
+			in.close();
+
+			for(float f : peaks){
+				System.out.println(f+",");
+			}
+
+			MusicData.setpeaks(peaks);
+
+		}
+		catch (Exception e) {
+			System.out.println("111111111111EXCEEEEEEEEEEEEEEEEEEPTION!!!" + e);
+		}
+
+		try {
+			InputStream fis = Gdx.files.internal("data/testsamples.ar").read();
+			ObjectInputStream in = new ObjectInputStream(fis);
+			@SuppressWarnings("unchecked")
+			ArrayList<float[]> samples = (ArrayList<float[]>)in.readObject();
+			in.close();
+			MusicData.setSamples(samples);
+
+		}
+		catch (Exception e) {
+			System.out.println("222222222222222EXCEEEEEEEEEEEEEEEEEEPTION!!!" + e);
+		}
+
+
 	}
 }
--- AudioRunnerAndroid/src/edu/mills/cs280/audiorunner/LoadMusic.java
+++ AudioRunnerAndroid/src/edu/mills/cs280/audiorunner/LoadMusic.java
@@ -5,6 +5,8 @@ import java.io.ObjectInputStream;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.badlogic.gdx.Gdx;
+
 import android.app.Activity;
 import android.content.Intent;
 import android.os.AsyncTask;
@@ -15,30 +17,30 @@ import android.widget.ProgressBar;
 import android.widget.Toast;
 
 public class LoadMusic extends Activity{
-	
+
 	public String musicFile;
 	private ProgressBar progressIndicator;
 	private LinearLayout linProgressBar;
-    
-	@Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.progress_bar);
-    	
-    	Bundle extras = getIntent().getExtras();
-    	if(extras !=null)
-    	{
-    		musicFile = extras.getString("song");
-    	}
-        
-    	linProgressBar = (LinearLayout) findViewById(R.id.lin_progress_bar);
-        linProgressBar.setVisibility(View.VISIBLE);
-        progressIndicator = (ProgressBar) findViewById(R.id.progressbar);
-        
-        new decodeMusic().execute();
 
-    }
-	
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.progress_bar);
+
+		Bundle extras = getIntent().getExtras();
+		if(extras !=null)
+		{
+			musicFile = extras.getString("song");
+		}
+
+		linProgressBar = (LinearLayout) findViewById(R.id.lin_progress_bar);
+		linProgressBar.setVisibility(View.VISIBLE);
+		progressIndicator = (ProgressBar) findViewById(R.id.progressbar);
+
+		new decodeMusic().execute();
+
+	}
+
 
 	private class decodeMusic extends AsyncTask<Void, Void, Void> {
 
@@ -49,38 +51,7 @@ public class LoadMusic extends Activity{
 			//songData.setTestable(true);
 			//MusicData.setpeaks(songData.returnPeaks());
 			
-			System.out.println("TRYING TO LOAD!!!!");
-			
-			try {
-		        FileInputStream fis = new FileInputStream("AudioRunnerAndroid/assets/data/testPeaks.ar");
-		        ObjectInputStream in = new ObjectInputStream(fis);
-		        @SuppressWarnings("unchecked")
-				List<Float> peaks = (List<Float>)in.readObject();
-		        in.close();
-		        
-		        for(float f : peaks){
-		        	System.out.println(f+",");
-		        }
-		        
-		        MusicData.setpeaks(peaks);
-		        
-		      }
-		      catch (Exception e) {
-		          System.out.println("111111111111EXCEEEEEEEEEEEEEEEEEEPTION!!!" + e);
-		      }
-		      
-		      try {
-			        FileInputStream fis = new FileInputStream("AudioRunnerAndroid/assets/data/testSamples.ar");
-			        ObjectInputStream in = new ObjectInputStream(fis);
-			        @SuppressWarnings("unchecked")
-					ArrayList<float[]> samples = (ArrayList<float[]>)in.readObject();
-			        in.close();
-			        MusicData.setSamples(samples);
-			        
-			      }
-			      catch (Exception e) {
-			          System.out.println("222222222222222EXCEEEEEEEEEEEEEEEEEEPTION!!!" + e);
-			      }
+			MusicData.loadBufferedData();
 			
 			return null;
 		}
@@ -88,23 +59,23 @@ public class LoadMusic extends Activity{
 		@Override
 		protected void onPostExecute(Void v) {
 			Toast.makeText(getBaseContext(), 
-                    "Decoding finished", 
-                    Toast.LENGTH_SHORT).show();
-			
+					"Decoding finished", 
+					Toast.LENGTH_SHORT).show();
+
 			Intent gameIntent = new Intent(getApplicationContext(), AudioRunnerActivity.class);
-            gameIntent.putExtra("song", musicFile);
+			gameIntent.putExtra("song", musicFile);
 			startActivityForResult(gameIntent, 0);
-            finish();
+			finish();
 		}
 
 		@Override
 		protected void onPreExecute() {
-			
+
 		}
 
 		@Override
 		protected void onProgressUpdate(Void... values) {
-			
+
 		}
 
 	}
