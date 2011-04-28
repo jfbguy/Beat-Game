3c4a5c5ed95d8de4bc739bfc002cbdd5807e69df
dlu <dlu@mills.edu>
2011-04-28 11:49:09 -0700

    Commit with high scores and settings!

---
+++ AudioRunnerAndroid/src/edu/mills/cs280/audiorunner/VolumeSeekBar.java
@@ -0,0 +1,56 @@
+package edu.mills.cs280.audiorunner;
+
+import android.app.Activity;
+import android.content.Context;
+import android.os.Bundle;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.media.AudioManager;
+
+public class VolumeSeekBar extends Activity{
+	
+	@Override
+	   public void onCreate(Bundle savedInstanceState) {
+	       super.onCreate(savedInstanceState);
+	       setContentView(R.layout.settings);
+	       setVolumeControlStream(AudioManager.STREAM_MUSIC);   //Where does music come from?
+	      
+	       SeekBar seekBar = (SeekBar)findViewById(R.id.seekbar);
+	       final TextView seekBarValue = (TextView)findViewById(R.id.seekbarvalue);
+	       
+	       AudioManager audioManager;	//What replaces AudioManager
+	      
+	       audioManager = (AudioManager)getSystemService(Context.AUDIO_SERVICE);
+	       int maxVolume = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+	       int curVolume = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+	       SeekBar volControl = (SeekBar)findViewById(R.id.seekbar);
+	       volControl.setMax(maxVolume);
+	       volControl.setProgress(curVolume);
+	       volControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+
+
+	      
+	       //seekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener(){
+
+	   @Override
+	   public void onProgressChanged(SeekBar seekBar, int progress,
+	     boolean fromUser) {
+	    
+	    seekBarValue.setText(String.valueOf(progress));
+	    //audioManager.setStreamVolume(AudioManager.STREAM_MUSIC, arg1, 0);
+	   }
+
+	   @Override
+	   public void onStartTrackingTouch(SeekBar seekBar) {
+		   //seekBarValue.setText(getString(R.string.seekbar_tracking_on));
+	   }
+
+	   @Override
+	   public void onStopTrackingTouch(SeekBar seekBar) {
+		   //seekBarValue.setText(getString(R.string.seekbar_tracking_off));
+
+	   }
+	       });
+
+	}
+}
